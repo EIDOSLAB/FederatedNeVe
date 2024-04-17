64cebe4d37f6a2879379dc4b0e03310b0485d82b
Gianluca Dalmasso <gianluca.dalmasso@edu.unito.it>
2024-04-17 15:56:29 +0200

    - updated run.sh to allow multiple runs with different seeds

--- src/run.sh
+++ src/run.sh
@@ -6,38 +6,50 @@ optimizer="--optimizer sgd"
 scheduler="--scheduler-name neve"
 neve_only_ll="--neve-only-ll 1"
 model_use_groupnorm="--model-use-groupnorm 1"
-seed="--seed 0"
 dataset="--dataset-name cifar10"
 model="--model-name resnet18"
 
 # Numero di clients da avviare
-num_clients=20
+num_clients=10
 clients="--num-clients $num_clients"
 
-# Avvia il server
-echo "Avvio del server con parametri: $params $clients $optimizer $scheduler $neve_only_ll $model_use_groupnorm $seed $dataset $model"
-python server.py $params $clients $optimizer $scheduler $neve_only_ll $model_use_groupnorm $seed $dataset $model &
+number_of_seeds=10
+single_gpu=1
 
-# Ottieni l'ID del processo del server
-server_pid=$!
-
-# Aspettiamo che il server parta prima di far partire i processi figli (10 secondi dovrebbero bastare)
-sleep 10
-
-# Loop per avviare i clients
-for ((i=0; i<num_clients; i+=1))
+# Loop per le run con seed differente
+for ((current_seed=0; current_seed<number_of_seeds; current_seed+=1))
 do
-    # Determina se i è pari o dispari
-    is_even=$((i % 2 == 0))
+    seed="--seed $current_seed"
+    # Avvia il server
+    echo "Avvio del server con parametri: $params $clients $optimizer $scheduler $neve_only_ll $model_use_groupnorm $seed $dataset $model"
+    python server.py $params $clients $optimizer $scheduler $neve_only_ll $model_use_groupnorm $seed $dataset $model &
 
-    # Imposta CUDA_VISIBLE_DEVICES in base a is_even
-    if [ $is_even -eq 1 ]; then
-        gpu_id=0
-    else
-        gpu_id=1
-    fi
-    echo "Avvio del client $i con parametri: $params $clients $optimizer $scheduler $neve_only_ll $model_use_groupnorm $seed $dataset $model --current-client $i, sulla GPU: $gpu_id"
-    CUDA_VISIBLE_DEVICES=$gpu_id python client.py $params $clients $optimizer $scheduler $neve_only_ll $model_use_groupnorm $seed $dataset $model "--current-client" $i &
+    # Ottieni l'ID del processo del server
+    server_pid=$!
+
+    # Aspettiamo che il server parta prima di far partire i processi figli (10 secondi dovrebbero bastare)
+    sleep 10
+
+    # Loop per avviare i clients
+    for ((i=0; i<num_clients; i+=1))
+    do
+        # Determina se i è pari o dispari
+        is_even=$((i % 2 == 0))
+
+        # Imposta CUDA_VISIBLE_DEVICES in base a is_even
+        if [ $is_even -eq 1 ]; then
+            gpu_id=0
+        else
+            gpu_id=1
+        fi
+
+        if [ $single_gpu -eq 1]; then
+            gpu_id=0
+        fi
+
+        echo "Avvio del client $i con parametri: $params $clients $optimizer $scheduler $neve_only_ll $model_use_groupnorm $seed $dataset $model --current-client $i, sulla GPU: $gpu_id"
+        CUDA_VISIBLE_DEVICES=$gpu_id python client.py $params $clients $optimizer $scheduler $neve_only_ll $model_use_groupnorm $seed $dataset $model "--current-client" $i &
+    done
 done
 
 # Attendi che tutti i processi dei client siano completati
